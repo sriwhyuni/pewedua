8c13cecdbc7379eb6928cbc0544cf1e0b15ecba5
januari12 <januari.yanti@student.untan.ac.id>
2018-03-21 09:59:04 +0700

    latihan 1, halaman perdana

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//ini adalah komentar
+
+	echo "Sri Wahyuni<br>";
+	echo "H1101161004<br>";
+
+	echo "Hello world!<br>";
+	echo "Ini adalah Duniaku";
+	echo "Ini adalah Duniaku";
+
+	$sri=157;
+	echo "Tinggi badan adalah" .$sri. "cm";
+	
+?>
